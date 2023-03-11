ae8e144261aae3c8451d444cc3a6121ece45d284
Raziel Alron <raziel.alron@tipalti.com>
2023-03-11 21:11:19 +0200

    add postRoute to app.ts

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { userRoute } from './userRoute.js';
+import { postRoute } from './postRoute.js';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.get('/healtcheck', (req: Request, res: Response) : void => {
 });
 
 app.use('/user', userRoute);
+app.use('/post', postRoute);
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running on port: ${PORT}`);
