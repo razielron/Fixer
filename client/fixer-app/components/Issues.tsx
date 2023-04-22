2c93717c44f2d6c07d126c09b9fef8f3ccf03dd8
Raziel Alron <raziel.alron@tipalti.com>
2023-04-22 09:35:39 +0300

    small fix

--- client/fixer-app/components/Issues.tsx
+++ client/fixer-app/components/Issues.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Issue from './Issue';
-import Spinner from './spinner';
+import Spinner from './Spinner';
 import { issueClient } from '@/pages/api/issueClient';
 import { IssueModel } from '../src/models/issueModel.js';
 import { getCookie } from 'cookies-next';
@@ -11,30 +11,20 @@ export default function Issues() {
   const [allIssues, setAllIssues] = useState<IssueModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const posts: IssueModel[]  = [
-    {
+  const post: IssueModel = {
       id: 'id',
       autherId: 'razId',
       //autherName: 'raz',
       title: 'title',
       body: 'body',
       createdAt: new Date(),
-    },
-    {
-      id: 'id',
-      autherId: 'razId',
-      //autherName: 'raz',
-      title: 'title',
-      body: 'body',
-      createdAt: new Date(),
-    }
-  ];
+    };
 
   useEffect(() => {
     issueClient.getIssueByProfession('ELECTRICIAN', token)
       .then((issuesResponse) => {
         setIsLoading(false);
-        setAllIssues(issuesResponse.data || posts);
+        setAllIssues(issuesResponse.data || [post, post]);
       })
       .catch((err) => {
         console.error({err});
