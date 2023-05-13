2f1a47578abe4c77026012b99c34030aa465ce1d
Raziel Alron <raziel.alron@tipalti.com>
2023-05-13 09:58:36 +0300

    add comment route to app

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -6,6 +6,7 @@ import { authenticateUser } from "./apiAuthentication.js";
 import { userRoute } from './userRoute.js';
 import { postRoute } from './postRoute.js';
 import { issueRoute } from './issueRoute.js';
+import { commentRoute } from './commentRoute.js';
 import { s3Route } from './s3Route.js';
 
 dotenv.config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`) });
@@ -37,7 +38,8 @@ app.get('/userTokenCheck', authenticateUser, (req: Request, res: Response) : voi
 app.use('/user', userRoute);
 app.use('/post', postRoute);
 app.use('/issue', issueRoute);
-app.use('/s3', s3Route);
+app.use('/s3', commentRoute);
+app.use('/comment', s3Route);
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running on port: ${PORT}`);
