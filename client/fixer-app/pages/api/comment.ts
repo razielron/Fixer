c5474a2780fd34ff0b1cd4a057cd8a4f6e76bdc5
Raziel Alron <raziel.alron@tipalti.com>
2023-05-17 20:29:59 +0300

    fix prod issue

--- client/fixer-app/pages/api/comment.ts
+++ client/fixer-app/pages/api/comment.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { CommentModel } from '@/src/models/commentModel';
-import ApiResponseModel from '@/src/models/apiModel';
+import {ApiResponseModel} from '@/src/models/apiModel';
 import { commentClient } from '@/src/apiClients/commentClient';
 
 async function getCommentByIssueHandler (
