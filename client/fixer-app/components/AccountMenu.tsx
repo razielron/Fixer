a9d8f336ecd5b3dfac20a7c87ab55e91e4b82497
Raziel Alron <raziel.alron@tipalti.com>
2023-07-14 22:22:44 +0300

    add navigation to profile page

--- client/fixer-app/components/AccountMenu.tsx
+++ client/fixer-app/components/AccountMenu.tsx
@@ -8,20 +8,20 @@ interface AccountMenuProps {
 
 const AccountMenu: React.FC<AccountMenuProps> = ({name}) => {
     const router = useRouter();
+
     const signOut = async () => {
         deleteCookie('jwt_auth');
         deleteCookie('userInformation')
         router.push('/'); 
-    
     };
 
     return (
         <div className="bg-black w-56 absolute top-14 right-0 py-5 flext-col border-2 border-gray-800 flex">
             <div className="flex flex-col gap-3">
-                <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
+                <div className="px-3 group/item flex flex-row gap-3 items-center w-full" onClick={() => router.push('/profile')}>
                     <img className="w-8 rounded-md" src="/images/profile.jpg" alt="" />
                         <p className="text-white text-sm group-hover/item:underline">
-                            {name}
+                            {name ?? 'shachar'}
                         </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
