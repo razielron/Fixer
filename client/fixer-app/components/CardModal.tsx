dbe463001e7e3b74e0b352a2c59eeadcf3dfabb2
Raziel Alron <raziel.alron@tipalti.com>
2023-08-26 11:42:00 +0300

    fix scroll on card modal

--- client/fixer-app/components/CardModal.tsx
+++ client/fixer-app/components/CardModal.tsx
@@ -48,32 +48,34 @@ const CardModal: React.FC<Props> = (props) => {
   
   return (
     <>
-        <div className="mt-24 justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div className="flex justify-center items-center fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative mx-auto w-[60vw]">
                 {/*content*/}
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none" style={{ maxHeight: 'calc(100vh - 4rem)' }}>
                     {/*header*/}
-                    <div className="flow-root flex items-start justify-between p-2 border-b border-solid border-slate-200 rounded-t">
+                    <div className="flex items-start justify-between p-2 border-b border-solid border-slate-200 rounded-t">
                         <button onClick={props.hideModal} type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                             <span className="sr-only">Close menu</span>
                             <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
+                    </div>
+                    {/*body*/}
+                    <div className="relative flex-grow overflow-y-auto" style={{ maxHeight: 'calc(100vh - 6rem)' }}> {/* Adjust the maxHeight as needed */}
                         <Card cardData={props.cardData} isModalOpen={false} />
                         <div className="overflow-auto pl-5">
-                        {comments &&
+                            {comments &&
                             comments
                                 .sort((x, y) => sortByCreatedAt(x.createdAt, y.createdAt, false))
                                 .map((comment) => 
-                                    (<Comment comment={comment} />)
+                                (<Comment comment={comment} />)
                                 )
-                        }
+                            }
                         </div>
                         <div className="">
-                        <CommentForm onSubmit={handleNewComment} />
+                            <CommentForm onSubmit={handleNewComment} />
                         </div>
-                        
                     </div>
                 </div>
             </div>
